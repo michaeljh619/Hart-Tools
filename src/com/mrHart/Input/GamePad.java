4a705e3154b8c3a4fbff61790156ee3ca0cba870
michaeljh619 <michaeljh619@yahoo.com>
2015-05-02 14:11:48 -0700

    Added dark transparent sticks and buttons.

--- src/com/mrHart/Input/GamePad.java
+++ src/com/mrHart/Input/GamePad.java
@@ -4,9 +4,11 @@ public class GamePad {
 	// Named Constants
 	public static final int STYLE_DARK = 1;
 	public static final int STYLE_LIGHT = 2;
+	public static final int STYLE_DARK_TRANSPARENT = 3;
 	
 	public static final String DIR_DARK = "graphics/gamepad/dark/";
 	public static final String DIR_LIGHT = "graphics/gamepad/light/";
+	public static final String DIR_DARK_TRANSPARENT = "graphics/gamepad/dark_Transparent/";
 	
 	
 	/**
@@ -20,9 +22,13 @@ public class GamePad {
 		
 		case STYLE_DARK:
 			return DIR_DARK;
+			break;
 		case STYLE_LIGHT:
 			return DIR_LIGHT;
-		
+			break;
+		case STYLE_DARK_TRANSPARENT:
+			return DIR_DARK_TRANSPARENT;
+			break;
 		}
 		
 		return "";
